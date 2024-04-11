562b48caa60a0392ec9308190eae6dcda44c21c4
Kundan <2021itb049.kundan@students.iiests.ac.in>
2024-04-11 23:27:51 +0530

    added pwa support + robots.txt + sitemap.xml + google_ranking.html

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,26 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/Footer";
-
+import type { Viewport } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Convertfast",
   description: "Convertfast is a free online tool to convert and compress media files. No files are sent to any server - all conversions and compressions happen right on your machine.",
+  keywords : ["file converter", "file compressor", "compress files online", "file compress online", "convert files online", "file convert online", "convertfast", "convertfast online", "convertfast file converter", "convertfast file compressor", "convertfast file convert", "convertfast file compress", "compress images for forms", "compress photos", "jpeg to png", "jpg to png", "png to jpg", "png to svg", "mov to mp4"],
   openGraph :{
     images : 'https://r2.my-links.live/convertfast-og.png'
-  }
+  },
+  manifest : "/manifest.json",
 };
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+  ],
+}
+
 
 export default function RootLayout({
   children,
