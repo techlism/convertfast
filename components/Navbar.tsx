bcd68b2973519135366ba8e43faa2fb31f03f661
Kundan <2021itb049.kundan@students.iiests.ac.in>
2024-04-19 18:48:15 +0530

    added auto close on path change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,21 @@
 'use client'
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { Button } from './ui/button';
 import DarkModeSwitch from './DarkModeSwitch';
 import Image from 'next/image';
+import { Menu,X} from 'lucide-react';
+import { usePathname } from 'next/navigation'
 
 const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
-
+    const pathname = usePathname();
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
     };
+    useEffect(() => {
+        setIsOpen(false);
+    }, [pathname]);
 
     return (
         <nav className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -34,16 +39,12 @@ const Navbar: React.FC = () => {
                     <DarkModeSwitch/>                        
                     <div className="flex items-center">
                         <div className="-mr-2 flex md:hidden">
-                            <Button onClick={toggleNavbar} type="button" className="inline-flex items-center justify-center p-2" variant={'outline'}>
+                            <Button onClick={toggleNavbar} type="button" className="inline-flex items-center justify-center p-2 transition" variant={'outline'}>
                                 <span className="sr-only">Open main menu</span> {/*For accessibility*/}
                                 {!isOpen ? (
-                                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                                    </svg>
+                                    <Menu/>
                                 ) : (
-                                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
+                                    <X/>
                                 )}
                             </Button>
                         </div>                                                                    
@@ -53,9 +54,9 @@ const Navbar: React.FC = () => {
             {isOpen && (
                 <div className="md:hidden transition-slow">
                     <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        <Link href="#" className="  px-3 py-2 rounded-lg text-sm font-medium block">About</Link>
-                        <Link href="#" className="  px-3 py-2 rounded-lg text-sm font-medium block">Features</Link>
-                        <Link href="#" className="  px-3 py-2 rounded-lg text-sm font-medium block">Report</Link>
+                        <Link href="/about" className="  px-3 py-2 rounded-lg text-sm font-medium block hover:underline">About</Link>
+                        <Link href="#" className="  px-3 py-2 rounded-lg text-sm font-medium block hover:underline">Features</Link>
+                        <Link href="/report" className="  px-3 py-2 rounded-lg text-sm font-medium block">Report an issue</Link>
                     </div>
                 </div>
             )}
