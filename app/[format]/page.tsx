b2d9fd9af27eecd5afc146d5e1a6def9f81dc447
techlism <2021itb049.kundan@students.iiests.ac.in>
2025-01-16 12:47:19 +0530

    added a fallback redirection for legacy routes

---
+++ app/[format]/page.tsx
@@ -0,0 +1,18 @@
+import { redirect, notFound } from "next/navigation";
+import { conversions } from "@/lib/conversion-formats";
+
+export default function Page({ params }: { params: { format: string } }) {
+	const pattern = /^[a-z0-9]+-to-[a-z0-9]+$/i;
+	if (!pattern.test(params.format)) return notFound();
+
+	const [from, to] = params.format.toLowerCase().split("-to-");
+	const conversion = conversions.find(
+		(c) => c.from.toLowerCase() === from && c.to.toLowerCase() === to,
+	);
+
+	if (conversion) {
+		redirect(`/convert/${params.format}`);
+	} else {
+		return notFound();
+	}
+}
--- next.config.mjs
+++ next.config.mjs
@@ -22,75 +22,75 @@ const nextConfig = {
       }
     ]
   },
-    async rewrites() {
-        return [
-          {
-            source: '/mp4-to-:format', // Matches any URL like /mp4-to-xxx
-            destination: '/mp4-to/:format' // Rewrites to /mp4-to/xxx
-          },
-          {
-            source: '/mov-to-:format', // Matches any URL like /mov-to-xxx
-            destination: '/mov-to/:format' // Rewrites to /mov-to/xxx
-          },
-          {
-            source: '/webm-to-:format', // Matches any URL like /webm-to-xxx
-            destination: '/webm-to/:format' // Rewrites to /webm-to/xxx
-          },
-          {
-            source: '/mkv-to-:format', // Matches any URL like /mkv-to-xxx
-            destination: '/mkv-to/:format' // Rewrites to /mkv-to/xxx
-          },
-          {
-            source: '/avi-to-:format', // Matches any URL like /avi-to-xxx
-            destination: '/avi-to/:format' // Rewrites to /avi-to/xxx
-          },          
-          {
-            source: '/flv-to-:format', // Matches any URL like /flv-to-xxx
-            destination: '/flv-to/:format' // Rewrites to /flv-to/xxx
-          },
-          {
-            source: '/png-to-:format', // Matches any URL like /png-to-xxx
-            destination: '/png-to/:format' // Rewrites to /png-to/xxx
-          },
-          {
-            source: '/jpg-to-:format', // Matches any URL like /jpg-to-xxx
-            destination: '/jpg-to/:format' // Rewrites to /jpg-to/xxx            
-          },
-          {
-            source: '/jpeg-to-:format', // Matches any URL like /jpeg-to-xxx
-            destination: '/jpeg-to/:format' // Rewrites to /jpeg-to/xxx
-          },
-          // {
-          // Dropping SVG support because of inkscape requirement issue
-          //   source: '/svg-to-:format', // Matches any URL like /svg-to-xxx
-          //   destination: '/svg-to/:format' // Rewrites to /svg-to/xxx            
-          // },
-          {
-            source : '/webp-to-:format',
-            destination : '/webp-to/:format'
-          },
-          {
-            source : '/bmp-to-:format',
-            destination : '/bmp-to/:format'
-          },
-          {
-            source : '/mp3-to-:format',
-            destination : '/mp3-to/:format'
-          },
-          {
-            source : '/wav-to-:format',
-            destination : '/wav-to/:format'
-          },
-          {
-            source : '/m4a-to-:format',
-            destination : '/m4a-to/:format'
-          },
-          {
-            source : '/aac-to-:format',
-            destination : '/aac-to/:format'
-          }
-        ];
-      },
+    // async rewrites() {
+    //     return [
+    //       {
+    //         source: '/mp4-to-:format', // Matches any URL like /mp4-to-xxx
+    //         destination: '/mp4-to/:format' // Rewrites to /mp4-to/xxx
+    //       },
+    //       {
+    //         source: '/mov-to-:format', // Matches any URL like /mov-to-xxx
+    //         destination: '/mov-to/:format' // Rewrites to /mov-to/xxx
+    //       },
+    //       {
+    //         source: '/webm-to-:format', // Matches any URL like /webm-to-xxx
+    //         destination: '/webm-to/:format' // Rewrites to /webm-to/xxx
+    //       },
+    //       {
+    //         source: '/mkv-to-:format', // Matches any URL like /mkv-to-xxx
+    //         destination: '/mkv-to/:format' // Rewrites to /mkv-to/xxx
+    //       },
+    //       {
+    //         source: '/avi-to-:format', // Matches any URL like /avi-to-xxx
+    //         destination: '/avi-to/:format' // Rewrites to /avi-to/xxx
+    //       },          
+    //       {
+    //         source: '/flv-to-:format', // Matches any URL like /flv-to-xxx
+    //         destination: '/flv-to/:format' // Rewrites to /flv-to/xxx
+    //       },
+    //       {
+    //         source: '/png-to-:format', // Matches any URL like /png-to-xxx
+    //         destination: '/png-to/:format' // Rewrites to /png-to/xxx
+    //       },
+    //       {
+    //         source: '/jpg-to-:format', // Matches any URL like /jpg-to-xxx
+    //         destination: '/jpg-to/:format' // Rewrites to /jpg-to/xxx            
+    //       },
+    //       {
+    //         source: '/jpeg-to-:format', // Matches any URL like /jpeg-to-xxx
+    //         destination: '/jpeg-to/:format' // Rewrites to /jpeg-to/xxx
+    //       },
+    //       // {
+    //       // Dropping SVG support because of inkscape requirement issue
+    //       //   source: '/svg-to-:format', // Matches any URL like /svg-to-xxx
+    //       //   destination: '/svg-to/:format' // Rewrites to /svg-to/xxx            
+    //       // },
+    //       {
+    //         source : '/webp-to-:format',
+    //         destination : '/webp-to/:format'
+    //       },
+    //       {
+    //         source : '/bmp-to-:format',
+    //         destination : '/bmp-to/:format'
+    //       },
+    //       {
+    //         source : '/mp3-to-:format',
+    //         destination : '/mp3-to/:format'
+    //       },
+    //       {
+    //         source : '/wav-to-:format',
+    //         destination : '/wav-to/:format'
+    //       },
+    //       {
+    //         source : '/m4a-to-:format',
+    //         destination : '/m4a-to/:format'
+    //       },
+    //       {
+    //         source : '/aac-to-:format',
+    //         destination : '/aac-to/:format'
+    //       }
+    //     ];
+    //   },
     async headers() {
       return [
         {
