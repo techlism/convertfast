b02940c1e38c31cb327d92a9044b07a31c382975
techlism <2021itb049.kundan@students.iiests.ac.in>
2025-01-18 09:57:01 +0530

    adding static params to /[format]

--- app/[format]/page.tsx
+++ app/[format]/page.tsx
@@ -1,6 +1,12 @@
 import { redirect, notFound } from "next/navigation";
 import { conversions } from "@/lib/conversion-formats";
 
+export async function generateStaticParams() {
+	return conversions.map((conversion) => ({
+		format: `${conversion.from.toLowerCase()}-to-${conversion.to.toLowerCase()}`,
+	}));
+}
+
 export default function Page({ params }: { params: { format: string } }) {
 	const pattern = /^[a-z0-9]+-to-[a-z0-9]+$/i;
 	if (!pattern.test(params.format)) return notFound();
